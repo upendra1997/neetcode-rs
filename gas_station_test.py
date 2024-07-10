c2d471ed501e32d377cafcc02bf68e8c77d445ec
Upendra Upadhyay <upendra.upadhyay.97@gmail.com>
2024-07-10 10:26:45 +0530

    fix

--- gas_station_test.py
+++ gas_station_test.py
@@ -6,8 +6,8 @@ class Solution:
         res = 0
         for i in range(len(gas)):
             total += gas[i] - cost[i]
-            
             if total < 0:
                 total = 0
                 res = i + 1
+        return res
 
